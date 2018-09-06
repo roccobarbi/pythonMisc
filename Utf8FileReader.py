c3a11d296f710911a11d0c39496caf2460212cbc
Rocco Barbini <roccobarbi@gmail.com>
2018-09-06 17:30:16 +0200

    read function renamed to map to make it more clear what it does, repeated code simplified

--- Utf8FileReader.py
+++ Utf8FileReader.py
@@ -1,21 +1,20 @@
 class Utf8FileReader:
     
     def __init__(self, source):
-        self.source = source
+        self._source = source
 
     # reads the file and applies the function fun to each line
     # if acc is defined as a list, it appends the result of fun to the list
     # if acc is null, it only runs fun on each line
-    def read(self, fun, acc = None):
+    def map(self, fun, acc = None):
+        file = open(self._source, 'r', encoding="utf-8")
         if (acc is None):
             # there is no accumulator
-            file = open(self.source, 'r', encoding="utf-8")
             for line in file:
                 fun(line)
             file.close()
         else:
             # there is an accumulator
-            file = open(self.source, 'r', encoding="utf-8")
             for line in file:
                 acc.append(fun(line))
             file.close()
